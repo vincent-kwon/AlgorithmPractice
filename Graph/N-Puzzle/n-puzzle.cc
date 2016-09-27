c776ac65e15738b95484e333cae9d39f6eca50fd
Hobum <hobum.kwon@gmail.com>
2016-09-28 08:58:59 +0900

    Add bfs search for N-Puzzle

--- Graph/N-Puzzle/n-puzzle.cc
+++ Graph/N-Puzzle/n-puzzle.cc
@@ -1,13 +1,116 @@
 #include <iostream>
+#include <queue>
+#include <vector>
+#include <stdlib.h>
+#include <map>
 
-using namepsace std;
+using namespace std;
+
+#define MAX_VAL 16
 
 class State {
-  vector<State> getAdjacent() const;
+ public: 
+  vector<State> getAdjacent() {
+    vector<State> v;
+    int index = getZero();
+    //swap top
+    if (index >= 0 && index <= 3) {}
+    else {
+      swap(index-4, index);
+      State ns;
+      ns.setArray(a);
+      v.push_back(ns);
+      swap(index-4, index);
+    } 
+    //swap bottom
+    if (index >= 12 && index <= 15) {}
+    else {
+      swap(index+4, index);
+      State ns;
+      ns.setArray(a);
+      v.push_back(ns);
+      swap(index+4, index);
+    }
+    //swap left
+    if (index == 0 || index == 4 || index == 8 || index == 12) {}
+    else {
+      swap(index-1, index);
+      State ns;
+      ns.setArray(a);
+      v.push_back(ns);
+      swap(index-1, index);
+    }
+    //swap right
+    if (index == 3 || index == 7 || index == 11 || index == 15) {} 
+    else {
+      swap(index+1, index);
+      State ns;
+      ns.setArray(a);
+      v.push_back(ns);
+      swap(index+1, index);
+    }
+    return v;
+  }
 
-  bool operator < (const State& rhs) const;
+  bool operator< (const State& rhs) const {
+    for (int i = 0; i < MAX_VAL; i++) {
+      if (a[i] < rhs.a[i]) return true;
+      else if (a[i] > rhs.a[i]) return false;
+      else continue;
+    }
+    return false;
+  }
 
-  bool operator = (const State& hrs) const;
+  bool operator> (const State& rhs) const {
+    for (int i = 0; i < MAX_VAL; i++) {
+      if (a[i] > rhs.a[i]) return true;
+      else if (a[i] < rhs.a[i]) return false;
+      else continue;
+    }
+    return false;
+  }
+
+  bool operator() (const State& lhs, const State& rhs) const {
+    //cout << "Yes called () " << endl;
+    if(lhs == rhs) return true;
+    else return false;
+  }
+
+  bool operator==(const State& rhs) const {
+    //cout << "Yes called ==" << endl;
+    for (int i = 0; i < MAX_VAL; i++) {
+      if (a[i] != rhs.a[i]) {
+         //cout << "   false " << endl;
+        return false;
+      }
+    }
+    return true;
+  }
+  void setArray(int *tmp) {
+    for (int i = 0; i < MAX_VAL; i++) {
+      a[i] = tmp[i];
+    }
+  }
+
+  int getZero() {
+    for (int i = 0; i < MAX_VAL; i++) {
+      if (a[i] == 0) return i;
+    }
+    return -1;
+  }
+
+  void print() {
+    for (int i = 0; i < MAX_VAL; i++) {
+      cout << a[i] << ", ";
+    }
+    cout << "" << endl;
+  }
+  void swap(int first, int second) {
+    int tmp = a[first];
+    a[first] = a[second];
+    a[second] = tmp;
+  }
+  int a[MAX_VAL];
 };
 
 typedef map<State, int> StateMap;
@@ -25,13 +128,13 @@ int incr(int x) {
 int bidirectional(State start, State finish) {
   map<State, int> c;
   queue<State> q;
-  
+
   if (start == finish) return 0;
 
   q.push(start); c[start] = 1;
   q.push(finish); c[finish] = -1;
 
-  while (!.q.empty()) {
+  while (!q.empty()) {
     State here = q.front();
     q.pop();
     vector<State> adjacent = here.getAdjacent();
@@ -50,6 +153,7 @@ int bidirectional(State start, State finish) {
 }
 
 int bfs(State start, State finish) {
+
   if (start == finish) {
     return 0;
   }
@@ -57,19 +161,21 @@ int bfs(State start, State finish) {
   StateMap c;
 
   queue<State> q;
+
+  q.push(start); c[start] = 1;
  
-  while (!.q.empty()) {
+  while (!q.empty()) {
     State here = q.front();
     q.pop();
     int cost = c[here];
-
     vector<State> adjacent = here.getAdjacent();
     for (int i = 0; i < adjacent.size(); ++i) {
+      //adjacent[i].print();
       if (c.count(adjacent[i]) == 0) {
         if (adjacent[i] == finish) return cost + 1;
         c[adjacent[i]] = cost + 1;
         q.push(adjacent[i]);
-      }
+      } else cout << "Visitied!!!" << endl;
     }
   }
 
@@ -77,5 +183,17 @@ int bfs(State start, State finish) {
 }
 
 int main() {
-  
+  State s;
+  State e;
+
+  int a[] = { 15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0 };
+  int b[] = { 15,14,13,12,11,10,9,8,7,6,5,4,3,0,2,1 };
+//  int b[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };    
+  s.setArray(a);
+  e.setArray(b);
+  int r = bfs(s,e);
+
+  cout << "r: " << r << endl;
+
+  return 0;
 }
