2e7951c2afb9c58aa55aaeeedfab6e5f0c216bd7
Hobum <hobum.kwon@gmail.com>
2016-11-11 07:45:56 +0900

    Add floyd-warshall solution

---
+++ BasicDataStructure/graph/FloydWarshall/floy.cc
@@ -0,0 +1,75 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  floy.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2016년 11월 11일 07시 05분 09초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hobum (Vincent) Kwon (mn), hobum.kwon@gmail.com
+ *        Company:  Samsung Electornics
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+#define VC_MAX 4
+#define INF 987544321
+using namespace std;
+
+int m[VC_MAX][VC_MAX] = {
+  {0,  3,  6, 15},
+  {INF, 0, -2, INF},
+  {INF, INF, 0, 2},
+  {1,  INF, INF, 0},
+};
+
+int p[VC_MAX][VC_MAX] = {
+  {-1, 0, 0, 0},
+  {-1, -1, 1, -1},
+  {-1, -1, -1, 2},
+  {3, -1, -1, -1}, 
+};
+
+void printArray() {
+  for (int i = 0; i < VC_MAX; i++) {
+    for (int j = 0; j < VC_MAX; j++) {
+      cout << i << "," << j << ":" << m[i][j] << endl;
+    }
+  }
+}
+
+void printPath() {
+  for (int i = 0; i < VC_MAX; i++) {
+    for (int j = 0; j < VC_MAX; j++) {
+      cout << i << "," << j << ":" << p[i][j] << endl;
+    }
+  }
+}
+void getFloyWarshall() {
+
+  for (int k = 0; k < VC_MAX; k++)
+    for (int i = 0; i < VC_MAX; i++) 
+      for (int j = 0; j < VC_MAX; j++) {
+        cout << i << "," << j << "," << k << ":" << m[i][j] << "," << m[i][k] << "," << m[k][j] << endl;
+        if (m[i][k] == INF || m[k][j] == INF) continue;
+        if (m[i][j] > (m[i][k] + m[k][j])) {
+          m[i][j] = m[i][k] + m[k][j];
+          p[i][j] = p[k][j];
+          cout << "   swap " << endl;
+        }
+      }
+}
+
+int main() {
+  getFloyWarshall();
+  printArray();
+  cout << "print path: " << endl;
+  printPath();
+  return 0;
+}
