e47f2bfa1a4122c7430def08193a35c1a544bf66
Hobum <hobum.kwon@gmail.com>
2016-11-24 06:06:08 +0900

    Add two robot delivery

--- DynamicProgramming/Postman/post_delivery.cc
+++ DynamicProgramming/Postman/post_delivery.cc
@@ -25,10 +25,10 @@
 using namespace std;
 
 int Map[100][100];
-int Floy[100][100];
 int go[10];
 int stop[10];
 int N,E,T;
+int dp[100][1024];
 
 int getBitCount(int i) {
   int count = 0;
@@ -39,20 +39,17 @@ int getBitCount(int i) {
   return count;
 }
 
-int tsp(int startNode, int maxBit, int value, int endNode) {
-  int numBit = getBitCount(value);
-  int ret = VC_MAX;
-  
-  if (value == 0) return Map[startNode][endNode];
-  
+int tsp(int startNode, int maxBit, int value) {
+  int& ret = dp[startNode][value];
+  if (ret != VC_MAX) return ret;
+  if (value == 0) return Map[startNode][1];
   for (int i = 0; i < maxBit; i++) {
     int bitMask = 1 << i;
-
     if (bitMask & value) {
       int newValue = ~bitMask & value;      
       int tmp = Map[startNode][go[i]] + 
                 Map[go[i]][stop[i]] +
-                tsp(stop[i], maxBit, newValue, endNode);
+                tsp(stop[i], maxBit, newValue);
       ret = min(ret, tmp);
     }
   }
@@ -68,11 +65,15 @@ int main() {
     cin >> N;
     cin >> E;
     cin >> T;
-
+    for (int i = 1; i <= N; i++) {
+      for (int k = 1; k <= 1024; k++) {
+        dp[i][k-1] = VC_MAX;
+      }
+    }    
     for (int i = 1; i <= N; i++) {
       for (int j = 1; j <= N; j++) {
-        Floy[i][j] = VC_MIN; //init
-        Map[i][j] = VC_MAX;
+        if (i != j) Map[i][j] = VC_MAX;
+        else Map[i][i] = 0;
       }
     }    
     for (int i = 1; i <= E; i++) {
@@ -81,7 +82,6 @@ int main() {
       cin >> e;
       cin >> cost;
       Map[s][e] = cost;
-      cout << s << "," << e << ":" << cost << endl;
     }
     for (int i = 0; i < T; i++) {
       int s, e;
@@ -90,28 +90,26 @@ int main() {
       go[i] = s;
       stop[i] = e;
     }
-    // calculate floyd-marshal
     for (int k = 1; k <= N; k++) {
       for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-          if ((Map[i][k] != VC_MAX) && (Map[k][j] != VC_MAX)) { 
-            if (Map[i][j] > Map[i][k] + Map[k][j]) Map[i][j] = Map[i][k] + Map[k][j];
-          }
+          if (Map[i][j] > (Map[i][k] + Map[k][j])) Map[i][j] = Map[i][k] + Map[k][j];
         }
       }
     }
-    // iterate all set
     const int bits = (1 << T) - 1; 
     int tmp = T; 
     int ret = VC_MAX;
-    
-    while (tmp > T/2) {
+    int bitCount = getBitCount(bits);
+    int border = (tmp + 1)/2;
+    while (tmp >= border) {
       int decBits = bits;
-      while (decBits > 0) {
+      int innerBorder = (decBits+1)/2;
+      while (decBits >= innerBorder) {
         if (getBitCount(decBits) == tmp) {
-          int ret1 = tsp(1, getBitCount(bits), decBits, 1); 
           int antibits = ~decBits & bits;
-          int ret2 = tsp(1, getBitCount(bits), antibits, 1);
+          int ret1 = tsp(1, bitCount, decBits); 
+          int ret2 = tsp(1, bitCount, antibits);
           int tmp = max(ret1, ret2);
           ret = min(ret, tmp);
         }
