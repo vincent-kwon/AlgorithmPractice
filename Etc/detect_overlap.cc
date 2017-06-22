091dd07d3eb30964d9ab1bc65cafb628c7730098
Hobum <hobum.kwon@gmail.com>
2017-06-23 07:02:00 +0900

    Adding overlap check

--- Etc/detect_overlap.cc
+++ Etc/detect_overlap.cc
@@ -19,64 +19,37 @@
 #include <iostream>
 using namespace std;
 
+typedef struct _rect {
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+} Rect;
+
 int main() {
-  int one[4][2] = {
-    0,0,
-    10,0,
-    0,10,
-    10,10
-   };
-
-  int two[4][2] = {
-    10,0,
-    15,0,
-    10,15,
-    15,15,
-  };
-
-  bool result = true;
-  bool x_overlap = false;
-  bool y_overlap = false;
-
-  //if (one[0][0] < two [0][0]) 
-
-  // 1. identical (easy)
-  if (one[0][0] == two[0][0] &&
-      one[0][1] == two[0][1] &&
-      one[1][0] == two[1][0] &&
-      one[1][1] == two[1][1]) {
-      return true;
-  } 
- 
-  int **left;
-  int **right;
-  int **top;
-  int **bottom;
-
-  bool width_lap = false;
-  bool height_lap = false;
-
-  // test width overlap
-  if (one[0][0] <= two[0][0]) {
-    if (one[1][0] > two[0][0]) width_lap = true;
-    else width_lap = false;   
-  }
-  else {
-    if (two[1][0] > one[0][0]) width_lap = true;
-    else width_lap = false;   
-  }
-  if (one[0][1] <= two[0][1]) {
-    if (one[2][1] > two[2][1]) height_lap = true;
-    else height_lap = false;   
-  }
-  else {
-    if (two[2][1] < one[2][1]) height_lap = true;
-    else height_lap = false;   
+  Rect rect[2];
+  rect[0].x1 = 0;
+  rect[0].y1 = 0;
+  rect[0].x2 = 10;
+  rect[0].y2 = 10;
+  
+  rect[1].x1 = 9;
+  rect[1].y1 = 0;
+  rect[1].x2 = 15;
+  rect[1].y2 = 15;
+  
+  // check first's rigth < second's left
+  // 1. if first < second, can be, if true, never overlap
+  // 2. if second < first, cant' be, always false
+  if (rect[0].x2 <= rect[1].x1 || rect[1].x2 <= rect[0].x1) {
+    cout << "No" << endl;
+    return false;
   }
 
-  if (width_lap == true && height_lap == true)  result = true;
-  else result = false;
-
-  cout << "Overlap: " << result << endl;
+  if (rect[0].y2 <= rect[1].y1 || rect[1].y2 <= rect[0].y1) {
+    cout << "No" << endl;
+    return false;
+  }
+  cout << "Yes " << endl;
   return 0;
 }
