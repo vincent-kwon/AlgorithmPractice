0bf9531f892444edcfa2c857305951e0ad05567e
Hobum <hobum.kwon@gmail.com>
2017-06-30 15:12:03 +0900

    Adding 1g1m re-written

--- BasicDataStructure/graph/Bellman-Ford/bellford2.cc
+++ BasicDataStructure/graph/Bellman-Ford/bellford2.cc
@@ -20,9 +20,10 @@
 
 using namespace std;
 
-#define NODE_NUM 5
+#define NODE_NUM 4
 #define INF 987654321
 
+#if 0
 int m[NODE_NUM][NODE_NUM] = {
  // 0, 1,  2, 3, 4, 5
   { 0, 4,  5, 8, 0 },
@@ -32,16 +33,40 @@ int m[NODE_NUM][NODE_NUM] = {
   { 0, 0,  0, 1, 0 },
 };
 
-int dt[NODE_NUM] = {0, INF, INF, INF, INF};
+     1        -2   
+  0 ---> 1 <-------
+  |      ^        | 
+  |      |-7      |
+   ----> 2------->3
+     3       -6
+
+  0 = 0 
+  1 = -5
+  2 = 3
+  3 = -3
+  
+#endif
+
+int m[NODE_NUM][NODE_NUM] = {
+  { 0,  1, 3,  0 },
+  { 0,  0, 0,  0 },
+  { 0, -7, 0, -6 }, 
+  { 0, -2, 0,  0 } 
+};
+
+int dt[NODE_NUM] = {0, INF, INF, INF};
 
 // TODO: negative cycle
 //
 int main() {
-  for (int i = 0; i < NODE_NUM; i++) {
-    for (int j = 0; j < NODE_NUM; j++) {
-      if (m[i][j] != 0 && (dt[j] > (dt[i] + m[i][j]))) {
-        dt[j] = dt[i] + m[i][j];
-      }
+  for (int node = 0; node < NODE_NUM-1; node++)
+    for (int i = 0; i < NODE_NUM; i++) {
+      for (int j = 0; j < NODE_NUM; j++) {
+        cout << i << " -> " << j << " : m[i][j] = " << m[i][j] << " dt[j] : " << dt[j] << ", dt[i] + m[i][j] = " << (dt[i] + m[i][j]) << endl;
+        if (m[i][j] != 0 && (dt[j] > (dt[i] + m[i][j]))) {
+          dt[j] = dt[i] + m[i][j];
+        }
+        else cout << i << " -> " << j << " no path " << endl;
     }
   }
   for (int i = 0; i < NODE_NUM; i++) cout << dt[i] << endl;
