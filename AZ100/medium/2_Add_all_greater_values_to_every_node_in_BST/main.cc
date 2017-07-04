26e5d6eaca119be199ee9dd450be69b8be7823cb
Hobum <hobum.kwon@gmail.com>
2017-07-05 06:33:04 +0900

    Solving 1, 2 of medium

--- AZ100/medium/2_Add_all_greater_values_to_every_node_in_BST/main.cc
+++ AZ100/medium/2_Add_all_greater_values_to_every_node_in_BST/main.cc
@@ -22,6 +22,7 @@ using namespace std;
 
 typedef struct _Node {
   int value;
+  int sum;
   struct _Node *left;
   struct _Node *right;
 } Node;
@@ -50,39 +51,60 @@ bool isBst(Node* node, int& min, int& max) {
   return true;
 }
 Node* root;
-/*
- *       21
- *    4      35
- * 2    30 
+
+int lastVisitSum = 0;
+
+void reverseInorderTraverse(Node* node) { // return sum value of next big
+  if (node == NULL) return;
+  
+  // sum = lower bigger + upper bigger
+  reverseInorderTraverse(node->right);
+  lastVisitSum = node->value + lastVisitSum;
+  cout << "visit: " << lastVisitSum << endl;
+  node->sum = lastVisitSum;
+  reverseInorderTraverse(node->left);
+}
+/* when going right, value to get is next close node's sum
+ * when going left,  
+ *
+ *
+ *        50
+ *    30      70
+ *  20   40  
+ * 
+ *  should be modified to 
+ *
+ *       120
+ *    190    70     
+ * 210   160
  */
+
 int main() {
   // recursively 
   //  A
   // B C => subtreeMax(left) max <= A && A subtreeMin(right) 
   //
   root = new Node();
-  root->value = 21;
+  root->value = 50;
   root->left = new Node();
   root->right = new Node();
 
   Node* level2_left = root->left;
-  level2_left->value = 4;
+  level2_left->value = 30;
 
   Node* level2_right = root->right;  
-  level2_right->value = 35;
+  level2_right->value = 70;
   level2_right->left = level2_right->right = NULL;
 
   level2_left->left = new Node();
-  level2_left->left->value = 2;
+  level2_left->left->value = 20;
   level2_left->left->left = level2_left->left->right = NULL;
 
   level2_left->right = new Node();
-  level2_left->right->value = 8;
+  level2_left->right->value = 40;
   level2_left->right->left = level2_left->right->right = NULL;
  
-  int m, n;
-
-  if (isBst(root, m, n)) cout << "this is BST !!! " << endl;
-  else cout << "No shit" << endl;
+  cout << "reverse inorder " << endl;
+  reverseInorderTraverse(root);
   return 0; 
 }
