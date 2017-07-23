3653fb204defdd63b0d65cdc1669fc91a6823c3c
Hobum <hobum.kwon@gmail.com>
2017-07-23 02:45:24 -0700

    Add sumlink

---
+++ AZ100/most/sumlink/main.cc
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2017년 07월 23일 02시 37분 34초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hobum (Vincent) Kwon (mn), hobum.kwon@gmail.com
+ *        Company:  Samsung Electornics
+ *
+ *
+ * Adding each link, if link is bigger than 10, than push it into stack
+ *
+ * when create a node, with value of sum of each link + stack top/pop and then if this value is >= 10, then save it to stack
+ * when linked list finished, if stack is not empty, adding a new link with 1
+ *
+ * instead of stack, just int carry = 0;
+ *
+ * Also do not forget one can be shorter than one 
+ *
+ *
+ *
+ * =====================================================================================
+ */
+
+
+// memo: consider length difference
+
