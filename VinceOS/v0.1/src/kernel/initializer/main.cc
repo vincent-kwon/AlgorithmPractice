f825b8f7ee4a8392dfad95954138d666cafa0a46
Hobum <hobum.kwon@gmail.com>
2017-08-13 00:15:14 -0700

    Adding shared memory

--- VinceOS/v0.1/src/kernel/initializer/main.cc
+++ VinceOS/v0.1/src/kernel/initializer/main.cc
@@ -16,16 +16,46 @@
  * =====================================================================================
  */
 #include <iostream>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
 #include "Process.h"
+#include <stdio.h>
+
 using namespace std;
+
+void* create_shared_memory(size_t size) {
+  int protection = PROT_READ | PROT_WRITE;
+  int visibility = MAP_ANONYMOUS | MAP_SHARED;
+  return mmap(NULL, size, protection, visibility, 0,0);
+}
 
 int main() {
   Process p;
+  char parent_message[256] = "Hello";
+  char child_message[256] = "goodbye";
+  void * shmem = create_shared_memory(128);
+  cout << "size of char* : " << sizeof(child_message) << endl;
+  memcpy(shmem, parent_message, 256);
+  int pid = fork();
+  if (pid == 0) {
+    cout << "child read: " << static_cast<char*>(shmem) << endl;
+    memcpy(shmem, child_message, 256);
+  }
+  else {
+    //cout << "parent read: " << static_cast<char*>(shmem) << endl;
+    printf("Parent read: %s\n", shmem);
+    sleep(1);
+    cout << "After 1s parent read: " << static_cast<char*>(shmem) << endl;
+  }
+#if 0
   cout << "[main] what main system init may need ?? " << endl;
   cout << "Initialize scheduler/process manager... " << endl;
   cout << "Initialize memory manager... " << endl;
   cout << "Initialize file manager... " << endl;
   p.info();
   cout << "[main] end...." << endl;
+#endif
   return 0;
 }
