06cce4d8a5f41bbf783ba4ea2ec36caecb027de8
Hobum <hobum.kwon@gmail.com>
2017-08-13 05:18:22 -0700

    Added shared memory with mmap and shm

--- VinceOS/v0.1/src/kernel/initializer/main.cc
+++ VinceOS/v0.1/src/kernel/initializer/main.cc
@@ -22,14 +22,62 @@
 #include <string.h>
 #include "Process.h"
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
 #include "../utility/timestamper.h"
 
 using namespace std;
+
+union semun {
+  int val;
+  struct semid_ds *buf;
+  unsigned short int *array;
+  struct seminfo *__buf;
+};
 
 void* create_shared_memory(size_t size) {
   int protection = PROT_READ | PROT_WRITE;
   int visibility = MAP_ANONYMOUS | MAP_SHARED;
   return mmap(NULL, size, protection, visibility, 0,0);
+}
+
+int binary_semaphore_allocation (key_t key, int sem_flags)
+{
+  return semget (key, 1, sem_flags);
+}
+
+int binary_semaphore_deallocate (int semid)
+{
+  union semun ignored_argument;
+  return semctl (semid, 1, IPC_RMID, ignored_argument);
+}
+
+int binary_semaphore_initialize (int semid)
+{
+  union semun argument;
+  unsigned short values[1];
+  values[0] = 1;
+  argument.array = values;
+  return semctl (semid, 0, SETALL, argument);
+}
+
+int binary_semaphore_wait (int semid)
+{
+  struct sembuf operations[1];
+  operations[0].sem_num = 0;
+  operations[0].sem_op = -1;
+  operations[0].sem_flg = SEM_UNDO;
+  return semop (semid, operations, 1);
+}
+
+int binary_semaphore_post (int semid)
+{
+  struct sembuf operations[1];
+  operations[0].sem_num = 0;
+  operations[0].sem_op = 1;
+  operations[0].sem_flg = SEM_UNDO;
+  return semop (semid, operations, 1);
 }
 
 int main() {
---
+++ VinceOS/v0.1/src/kernel/shared_memory/shared_memory_test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+
+int main ()
+{
+  int segment_id;
+  char* shared_memory;
+  struct shmid_ds shmbuffer;
+  int segment_size;
+  const int shared_segment_size = 0x6400; // memo-20170812: 1024 * (1 + 16 + 25) = 25600
+  // get shared memory
+  segment_id = shmget(IPC_PRIVATE, shared_segment_size, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+
+  // get actual memory buffer address
+  shared_memory = (char*)shmat(segment_id, 0, 0);
+  printf("shared memory attached at address %p\n", shared_memory);
+
+  // get size via shmid_ds (data structure) 
+  shmctl (segment_id, IPC_STAT, &shmbuffer);
+  segment_size = shmbuffer.shm_segsz;
+  printf("segment size: %d\n", segment_size);
+
+  // to the shared memory address, write hello work
+  sprintf(shared_memory, "Hello, world.");
+
+  // detach
+  shmdt(shared_memory);
+
+  // can attach to different ID 0x5000000
+  shared_memory = (char*) shmat (segment_id, (void*) 0x5000000, 0);
+  printf("shared memory reattached at address %p\n", shared_memory);
+  printf("%s\n", shared_memory);
+
+  // detach again
+  shmdt(shared_memory);
+
+  // remove??
+  shmctl(segment_id, IPC_RMID, 0);
+  return 0;
+}
